3215cdce59a162c94f827fb9a35d35337aae0ad3
oumnou <oumaimanouini08@gmail.com>
2024-02-10 11:17:32 +0100

    Creating Post Class

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+
+
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/post/post.dart
@@ -0,0 +1,10 @@
+class Post{
+
+  String userId;
+  int id;
+  int title;
+  String body;
+
+  Post({required this.userId,required this.id,required this.title,required this.body});
+  
+}
